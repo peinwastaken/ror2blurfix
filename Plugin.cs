695f9e5553cbb12f507a682e4aa72eac5d8b291e
peinwastaken <87280510+peinwastaken@users.noreply.github.com>
2024-08-28 12:55:24 +0300

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace ror2blurfix
+{
+    [BepInPlugin("com.pein.rorblurfix", "Risk of Rain 2 Blur fix", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static new ManualLogSource Logger;
+        public Camera GetCamera() => Camera.main;
+        public PostProcessLayer GetPPLayer() => GetCamera().GetComponent<PostProcessLayer>();
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+
+            var harmony = new Harmony("com.pein.rorblurfix");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PostProcessLayer), "RenderFinalPass")]
+    public class PostProcessLayerOnEnablePatch
+    {
+        public static PostProcessLayer GetPPLayer() => Camera.main.GetComponentInChildren<PostProcessLayer>();
+
+        static bool Prefix(PostProcessLayer __instance, PostProcessRenderContext context, int releaseTargetAfterUse, int eye)
+        {
+            __instance.antialiasingMode = PostProcessLayer.Antialiasing.None;
+            return true;
+        }
+    }
+}
